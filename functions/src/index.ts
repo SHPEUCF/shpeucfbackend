38cfa2b9c344d0483964d6d1e9a0b8e6c70cce25
Jeffrey Ramos <jeffjramos21@gmail.com>
2020-10-09 01:29:19 -0400

    please merge

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,6 +2,7 @@ import firebase from 'firebase-admin';
 import * as userService from './Controller/User';
 import * as eventService from './Controller/Event';
 import * as committeeService from './Controller/Committee';
+
 import * as memberService from './Controller/Member';
 
 const config = {
