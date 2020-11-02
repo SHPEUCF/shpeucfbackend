19262d4ab96e6b8f736cbba58a2db707d405a86d
Jeffrey Ramos <jeffjramos21@gmail.com>
2020-11-01 22:38:20 -0500

    merge fixes

--- functions/src/Controller/Committee.ts
+++ functions/src/Controller/Committee.ts
@@ -50,35 +50,11 @@ export const deleteCommitteeController = functions.https.onRequest(async (reques
 
 	await committeeRef.get().then((docSnapshot) => {
 		if (docSnapshot.exists) {
-<<<<<<< HEAD
-			deleteCommittee(committee)
+			deleteCommittee(id)
 				.then(() => Promise.resolve())
 				.catch(error => Promise.reject(error));
 
-			response.status(200).send('Good job');
-		}
-		else {
-			response.status(404).send('Error 404: Committee document not found');
-		}
-	});
-});
-
-export const changeCommitteeLevelController = functions.https.onRequest(async (request, response) => {
-	const committee: Committee = new Committee(request.body);
-	const committeeCollection = getCommitteeCollection();
-	const committeeDoc = committeeCollection.doc(committee.id);
-
-	await committeeDoc.get().then((docSnapshot) => {
-		if (docSnapshot.exists) {
-			changeCommitteeLevel(committee)
-				.then(() => Promise.resolve())
-				.catch(error => Promise.reject(error));
-
-			response.status(200).send('Good job');
-=======
-			deleteCommittee(id);
-			response.status(200).send("Good Job");
->>>>>>> 750e301520e2dedef3089c03e9988a16f511ba7d
+			response.status(200).send('Good Job');
 		}
 		else {
 			response.status(404).send('Error 404: Committee document not found');
