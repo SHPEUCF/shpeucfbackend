248a7d639b1f1d3d526a925711f94c8bdcddaf99
Jeffrey Ramos <jeffjramos21@gmail.com>
2020-11-14 21:10:38 -0500

    removed positionConverter code

--- functions/src/Services/Position.ts
+++ functions/src/Services/Position.ts
@@ -1,13 +1,13 @@
 import { getElectionsCollection } from '../Controller/Elections';
-import { Position, positionConverter } from '../Models/Position';
+import { Position } from '../Models/Position';
 import { firestore } from 'firebase-admin';
 
 export const deletePosition = (position : Position) => {
 	const electionsCollection = getElectionsCollection();
-	const convertedPosition = positionConverter.toFirestore(position);
+	const positionToRemove = firestore.FieldValue.arrayRemove({ ...position });
 
 	electionsCollection.get().then((QuerySnapshot) => {
-		(QuerySnapshot.docs[0]).ref.update({ positions: firestore.FieldValue.arrayRemove(convertedPosition) })
+		(QuerySnapshot.docs[0]).ref.update({ positions: positionToRemove })
 			.then(() => Promise.resolve())
 			.catch(error => Promise.reject(error));
 	})
