9260b522f93f7c43c1477bcd1296460b28da3865
Sebastian <sebaoctavio@gmail.com>
2020-11-22 22:38:03 -0500

    added the controller for approveApplication

--- functions/src/Controller/Elections.ts
+++ functions/src/Controller/Elections.ts
@@ -3,6 +3,9 @@ import * as functions from 'firebase-functions';
 import { db } from '../index';
 import { closeElections } from '../Services/Elections';
 import { openElections } from '../Services/Elections';
+import { approveApplication } from '../Services/Elections';
+import { Candidate } from '../Models/Candidate';
+
 
 export function getElectionsCollection() {
 	return db.collection('elections').withConverter(electionsConverter);
@@ -15,5 +18,12 @@ export const closeElectionsController = functions.https.onRequest((request, resp
 
 export const openElectionsController = functions.https.onRequest((request, response) => {
 	openElections();
+	response.status(200).send('Good Job');
+});
+
+export const approveApplication = functions.https.onRequest((request, response) => {
+	const candidateApplication: Candidate = new Candidate(request.body);
+
+	approveApplication(candidateApplication);
 	response.status(200).send('Good Job');
 });
